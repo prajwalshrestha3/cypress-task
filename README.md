ed8fee79737b7dfca8b11e378bcafc7e1fec498c
prajwalshrestha <prajwal.shrestha@infomentum.co.uk>
2022-02-19 19:29:50 +0000

    Addition to readme file

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # cypress-task
 
-test
+To run the swaglab tests, please change the URL to "https://www.saucedemo.com/" on the cypress.json file
+To run the API tests, please change the URL to "https://reqres.in/" on the cypress.json file
+
+Technical debt
+
+-  When selecting the items in the list for the UI test, the selection should be made at random as that would allow to check if any of the other items in the list have any bugs around them
+
+-  The organisation for code should be better to make it more readable and manageable
--- cypress/integration/examples/apiTest.spec.js
+++ cypress/integration/examples/apiTest.spec.js
@@ -1,38 +1,58 @@
-describe('Basic api Tests', () => {
-    let todoItem;
-    it('GET', () => {
-        cy.request('/api/users/2').as('userList');
-        cy.get('@userList').then(users => {
-            expect(users.status).to.eq(200);
-            //assert.isArray(users.body, 'User list here!')
-        });
-    });
+describe("Basic api Tests", () => {
+   it("GET", () => {
+      cy.request("/api/users/2").as("userList");
+      cy.get("@userList").then((response) => {
+         expect(response.status).to.eq(200);
+         expect(response.body).to.deep.equals({
+            data: {
+               id: 2,
+               email: "janet.weaver@reqres.in",
+               first_name: "Janet",
+               last_name: "Weaver",
+               avatar: "https://reqres.in/img/faces/2-image.jpg",
+            },
+            support: {
+               url: "https://reqres.in/#support-heading",
+               text: "To keep ReqRes free, contributions towards server costs are appreciated!",
+            },
+         });
+      });
+   });
 
-    it('DELETE', () => {
-        cy.request('DELETE', '/api/users/2').as('userList');
-        cy.get('@userList').then(users => {
-            expect(users.status).to.eq(204);
-            assert.isString(users.body, 'deleted!')
-            //assert.isArray(users.body, 'User list here!')
-        });
-    });
+   it("DELETE", () => {
+      cy.request("DELETE", "/api/users/2").as("userList");
+      cy.get("@userList").then((response) => {
+         expect(response.status).to.eq(204);
+      });
+   });
 
-    it('POST', () => {
-        cy.request('POST', '/api/users',{
-            "name": "prajwal",
-            "job": "tester",
-            "id": "773"
-        }).as('postUser');
-        cy.get('@postUser').then(users => {
-            expect(users.status).to.eq(201);
-            cy.wrap(users.body).should('deep.include', {
-                "name": "prajwal",
-                "job": "tester"
-            });
-        });
-        cy.request('/api/users/773').as('newuser');
-        cy.get('@newuser').then(newuser => {
-            cy.log(newuser);
-        })
-    });
-})
+   it("POST", () => {
+      cy.request("POST", "/api/users", {
+         name: "prajwal",
+         job: "tester",
+         id: "773",
+      }).as("postUser");
+      cy.get("@postUser").then((response) => {
+         expect(response.status).to.eq(201);
+         expect(response.body).to.deep.contains({
+            name: "prajwal",
+            job: "tester",
+            id: "773",
+         });
+      });
+   });
+
+   it("PATCH", () => {
+      cy.request("PATCH", "/api/users/2", {
+         name: "morpheus",
+         job: "zion resident",
+      }).as("userList");
+      cy.get("@userList").then((response) => {
+         expect(response.status).to.eq(200);
+         expect(response.body).to.deep.contains({
+            name: "morpheus",
+            job: "zion resident",
+         });
+      });
+   });
+});
